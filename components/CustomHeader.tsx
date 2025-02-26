3eb9f6c57d530d3ddc53013a638bf77cc471b793
fasaldana <fernando_s_98@hotmail.com>
2025-02-26 17:19:43 -0500

    Create a custom header component

---
+++ components/CustomHeader.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const CustomHeader: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Ionicons name="home" size={24} color="black" />
+      <Text style={styles.title}>BANCO</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#0f265c",
+    marginLeft: 10,
+  },
+});
+
+export default CustomHeader;
