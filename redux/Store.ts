4cc8e1fc772f37ffedb181fb288dd3df425f92d1
fasaldana <fernando_s_98@hotmail.com>
2025-02-27 13:21:45 -0500

    Add a middleware for the store

--- redux/Store.ts
+++ redux/Store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./product/ProductSlice";
+import { thunk } from "redux-thunk";
 
 const store = configureStore({
   reducer: {
     products: productReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
