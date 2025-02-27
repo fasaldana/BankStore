265f64be5e3600d30018d082de70c300f4b65a66
fasaldana <fernando_s_98@hotmail.com>
2025-02-27 00:24:40 -0500

    Create custom button

---
+++ components/CustomButton.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+type CustomButtonProps = {
+  text: string;
+  textColor: string;
+  backgroundColor: string;
+  onPress: () => void;
+};
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  text,
+  textColor,
+  backgroundColor,
+  onPress,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.button, { backgroundColor }]}
+    >
+      <Text style={[styles.text, { color: textColor }]}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default CustomButton;
